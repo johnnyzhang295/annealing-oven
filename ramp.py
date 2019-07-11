01c82503316f523efa295440f15272300317d652
JZ <jozh4204@colorado.edu>
2019-07-11 11:44:18 -0600

    cleaned up ramp code

--- ramp.py
+++ ramp.py
@@ -1,16 +1,14 @@
 import numpy as np
 
 class Ramp:
-	anneal_presets = {row[0]: row[1:] for row in (np.loadtxt('anneal_reference.txt', dtype=str))}
-
+	
 	def __init__(self, fname):
-		self.material, self.thickness = np.loadtxt(fname, dtype=str)
-		self.thickness = int(self.thickness)
+		self.loadProgrammable('anneal_plastic.txt')
 
 		self.ramp_up_time = self.anneal_presets[self.material][0]
 		self.setpoint = self.anneal_presets[self.material][1]
 		self.hold_time = self.anneal_presets[self.material][2]
-		self.cool_time = self.anneal_presets[self.material][3]
+		#self.cool_time = self.anneal_presets[self.material][3]
 
 		self.soak_period = self.determineSoakPeriod()
 		self.profile = self.createRampProfile()
@@ -25,3 +23,18 @@ class Ramp:
 
 	def createRampProfile(self):
 		pass
+
+	def loadProgrammable(self, fname):
+		self.material = np.loadtxt(fname, dtype=str, usecols=0).item()
+		self.thickness = np.loadtxt(fname, dtype=int, usecols=1).item()
+
+	def loadPresets(fname, endpoint):
+		anneal_list = {}
+		materials = np.loadtxt(fname, dtype=str, usecols=0)
+
+		for index,row in enumerate(np.loadtxt(fname,dtype=int, usecols=range(1,endpoint))):
+			anneal_list[materials[index]] = row
+
+		return anneal_list
+
+	anneal_presets = loadPresets('anneal_reference.txt', 5)
--- test_ramp.py
+++ test_ramp.py
@@ -2,4 +2,6 @@ from ramp import *
 
 ramp = Ramp('anneal_plastic.txt')
 
+print(ramp.anneal_presets)
+
 print(ramp.soak_period)
